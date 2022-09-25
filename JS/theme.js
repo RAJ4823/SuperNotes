ab588628fcb76812f6ffdedaf627d35c4e3420e6
halpaniraj.hr4823@gmail.com <halpaniraj.hr4823@gmail.com>
2022-09-25 10:32:34 +0530

    Revert some changes

--- JS/theme.js
+++ JS/theme.js
@@ -1,31 +1,37 @@
 ls = localStorage;
 
 //Color changing script
-let color = document.querySelector(`link[href="CSS/Themes/colors/black.css"]`);
+let color = document.querySelector(`link[href="CSS/Themes/colors/temp.css"]`);
 //Accent themes
-let colorName = ls.getItem("color");
-let __colors = "CSS/Themes/colors/";
+let rose = "CSS/Themes/colors/rose.css";      //1
+let gold = "CSS/Themes/colors/gold.css";      //2
+let purple = "CSS/Themes/colors/purple.css";  //3
+let green = "CSS/Themes/colors/green.css";    //4 
+let silver = "CSS/Themes/colors/silver.css";  //5
+let colorArr = [0, rose, gold, purple, green, silver];
+let oldColor = ls.getItem("color");
 
 //Color changer
-color.href = __colors + 'purple.css';
-if (colorName != null)
-    color.href = __colors + colorName + '.css';
+if (oldColor == null)
+    color.href = purple;
+else
+    color.href = colorArr[oldColor];
 
-function changeColor(colorName) {
-    color.href = __colors + colorName + '.css';
-    ls.setItem("color", colorName);
+function changeColor(i) {
+    color.href = colorArr[i];
+    ls.setItem("color", i);
 }
 
 //Theme Changer
 let themeLink = document.querySelector(`link[href="CSS/Themes/bootstrap-dark.css"]`);
-let __themes = "CSS/Themes/bootstrap-";
+let path = "CSS/Themes/bootstrap-";
 let themeTxt = document.getElementById("theme");
 
 //Previous Theme
-themeLink.href = __themes + ls.getItem("theme") + '.css';
+themeLink.href = path + ls.getItem("theme") + '.css';
 
 //Updating Previous Theme text
-if (ls.getItem('theme') == dark)
+if (ls.getItem('theme') == "dark")
     themeTxt.innerHTML = "Light Mode";
 else
     themeTxt.innerHTML = "Dark Mode";
@@ -35,13 +41,12 @@ function changeTheme() {
     themeTxt = document.getElementById("theme");
 
     if (themeTxt.innerHTML != "Light Mode") {
-        themeLink.href = __themes + "dark.css";
+        themeLink.href = path + "dark.css";
         themeTxt.innerHTML = "Light Mode";
         ls.setItem("theme", "dark");
     } else {
-        themeLink.href = __themes + "light.css";
+        themeLink.href = path + "light.css";
         themeTxt.innerHTML = "Dark Mode";
         ls.setItem("theme", "light");
     }
 }
-
